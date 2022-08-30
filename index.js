75caa8784e7e9171543dc4b66ee9c878ad0d1a81
Trevor <trevorperry143@gmail.com>
2022-08-30 13:56:01 -0500

    commit tree 39c5475999b417973fab880548838add213c354f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"]
+const birthday = []
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        birthday.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+        console.log(birthday)
+        debugger
+}
+return birthday
+}
+
+function countDown(x) {
+    do {console.log(x)
+    x--}
+    while (x >= 0)
+}
+countDown(10)
